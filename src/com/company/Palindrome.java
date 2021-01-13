a9407ab2c04b25411e14ea3ccfc06482c6669d72
Popescu <vlad.a.popescu@softvision.com>
2021-01-13 15:42:08 +0200

    Optimized palindrome

---
+++ src/com/company/Palindrome.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(optimizedSolution(12321));
+    }
+
+    static boolean optimizedSolution(int x) {
+        int revertedNumber = 0;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x = x /10;
+        }
+
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+
+}
